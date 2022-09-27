1a9cb964ebeb4187b546473dbbaa5582fd4102b0
Roman Mykhasiv <mihasivroman@gmail.com>
2022-09-27 09:58:24 +0300

    added changes for gh-pages

--- src/helpers/getImage.tsx
+++ src/helpers/getImage.tsx
@@ -1,5 +1,6 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 export const getImage = (src: IconProp) => {
   return <FontAwesomeIcon className={'icon-img'} icon={src}></FontAwesomeIcon>
